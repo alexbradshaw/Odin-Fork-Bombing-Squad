8c38c073b8cdf068e89cbedbe4135607b2deec8c
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-10-17 16:42:11 -0400

    set up server to parse json body and use routes

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
