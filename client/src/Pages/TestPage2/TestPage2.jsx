5c8f918bc875f603370a67b41198351424c36ee0
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-10-17 16:46:37 -0400

    set up test page 2

---
+++ client/src/Pages/TestPage2/TestPage2.jsx
@@ -0,0 +1,13 @@
+import './TestPage2.css'
+
+const TestPage2 = () => {
+    return (
+        <>
+            <div>
+                Test2
+            </div>
+        </>
+    );
+}
+
+export default TestPage2;
