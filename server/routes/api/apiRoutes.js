7cee5b2410a947c4cfd7bdcc48598389811041b8
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-10-26 01:00:28 -0400

    added controllers to api file

--- server/routes/api/apiRoutes.js
+++ server/routes/api/apiRoutes.js
@@ -1,8 +1,27 @@
 const router = require('express').Router();
-const { signup, login } = require('../../controllers/api/apiControllers')
+
+const { getUser } = require('../../controllers/api/userControllers');
+
+const { signup, login } = require('../../controllers/api/authControllers');
+
+const { 
+    createItem, 
+    getAllItems, 
+    getItem, 
+    updateItem, 
+    deleteItem
+} = require('../../controllers/api/itemControllers');
 
 router.post('/signup', signup);
-
 router.post('/login', login);
+
+router.post('/item', createItem)
+router.get('/items', getAllItems);
+router.route('/item/:id')
+.get(getItem)
+.put(updateItem)
+.delete(deleteItem);
+
+router.get('/user', getUser)
 
 module.exports = router;
