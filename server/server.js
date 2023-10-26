161809e36159225da76c82384dea550ea7efa58b
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-10-26 00:59:50 -0400

    added req.session for login

--- server/server.js
+++ server/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const session = require('express-session');
 const routes = require('./routes');
 const db = require('./config/connection');
+const URI = require('./config/URI');
+
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const store = new MongoDBStore({
+    uri: URI,
+    collection: 'sessions'
+});
+
+store.on('error', function(error) {
+    console.error(error);
+});
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.SECRET || "secret",
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 4
+    },
+    store: store,
+    resave: false,
+    saveUninitialized: false
+}));
 
 app.use(routes);
 
