7ba0e5bed2bb4349cc661ddb659eb80637d83b9f
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-10-25 23:07:05 -0400

    reverted to old routing, new one kinda slow

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,31 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
 import App from './App.jsx'
 import './index.css'
 
-import TestPage from './Pages/TestPage/TestPage';
-import TestPage2 from './Pages/TestPage2/TestPage2';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <h1>Wrong page!</h1>,
-    children: [
-      {
-        index: true,
-        element: <TestPage />
-      }, 
-      {
-        path: '/test2',
-        element: <TestPage2 />
-      }
-    ]
-  }
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
