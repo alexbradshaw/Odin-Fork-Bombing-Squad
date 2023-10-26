944dc057cdc447f70dbb94a60f83eaf494dcd718
alexbradshaw <31080674+alexbradshaw@users.noreply.github.com>
2023-10-25 23:07:24 -0400

    reverted routing, added navbar import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 
-import { Outlet } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import TestPage from './Pages/TestPage/TestPage';
+import TestPage2 from './Pages/TestPage2/TestPage2';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Outlet />
-    </>
+    <Router>
+      <div className='app'>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<TestPage/>}/>
+            <Route path='/test2' element={<TestPage2/>}/>
+          </Routes>
+      </div>
+    </Router>
   )
 }
 
