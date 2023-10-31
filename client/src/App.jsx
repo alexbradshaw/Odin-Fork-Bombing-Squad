bc7336030108962760b74bc584c1ebcaa2670813
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-10-31 18:26:20 -0400

    adding landing page to root, got rid of useState

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 
 import {
@@ -11,16 +10,16 @@ import TestPage from './Pages/TestPage/TestPage';
 import TestPage2 from './Pages/TestPage2/TestPage2';
 import Navbar from './Components/Navbar/Navbar';
 import Listings from './Pages/Listings/Listings';
+import Landing from './Pages/Landing/Landing';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Router>
       <div className='app'>
         <Navbar />
           <Routes>
-            <Route path='/' element={<TestPage/>}/>
+            <Route path='/' element={<Landing/>}/>
+            <Route path='/test' element={<TestPage/>}/>
             <Route path='/test2' element={<TestPage2/>}/>
             <Route path='/listings' element={<Listings/>}/>
           </Routes>
