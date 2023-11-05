278e79b989e69ffe532fb2398dfb4db7e5676f1d
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-11-05 01:43:40 -0400

    added /api/health GET route

--- server/routes/api/apiRoutes.js
+++ server/routes/api/apiRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-const { getUser } = require('../../controllers/api/userControllers');
 const { signup, login, logout } = require('../../controllers/api/authControllers');
 const { 
     createItem, 
@@ -9,6 +8,8 @@ const {
     updateItem, 
     deleteItem
 } = require('../../controllers/api/itemControllers');
+const { health } = require('../../controllers/api/serverController')
+const { getUser } = require('../../controllers/api/userControllers');
 
 //! * Means that it's an auth protected route 
 
@@ -20,5 +21,7 @@ router.get('/user', getUser) // GET* route to get the logged in user
 router.post('/item', createItem) // POST* route to create a new item
 router.get('/items', getAllItems); // GET route that returns all items in the database
 router.route('/item/:id').get(getItem).put(updateItem).delete(deleteItem); // route for GET, PUT*, and DELETE*, route has a req.params.id var
+
+router.get('/health', health); // GET route for server status (deployment website can check to see if it's still working)
 
 module.exports = router;
