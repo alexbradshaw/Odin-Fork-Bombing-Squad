1cb6cc82a82ccaf3fb029fca027253e3650e0ef2
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-11-07 16:02:44 -0500

    moved routes to main, added context here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css'
 
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-import TestPage from './Pages/TestPage/TestPage';
-import TestPage2 from './Pages/TestPage2/TestPage2';
 import Navbar from './Components/Navbar/Navbar';
-import Listings from './Pages/Listings/Listings';
-import Landing from './Pages/Landing/Landing';
+
+export const UserData = createContext();
 
 function App() {
 
@@ -23,17 +17,12 @@ function App() {
   });
 
   return (
-    <Router>
       <div className='app'>
-        <Navbar />
-          <Routes>
-            <Route path='/' element={<Landing/>}/>
-            <Route path='/test' element={<TestPage/>}/>
-            <Route path='/test2' element={<TestPage2/>}/>
-            <Route path='/listings' element={<Listings setData={setData} username={userData.username}/>}/>
-          </Routes>
+        <UserData.Provider value={{ userData, setData }}>
+          <Navbar />
+          <Outlet />
+        </UserData.Provider>
       </div>
-    </Router>
   )
 }
 
