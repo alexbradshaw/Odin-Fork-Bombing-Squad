fcb81f42bf28ca28d55a6e4e670e8abcf1438529
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-11-07 19:41:35 -0500

    updated URI path

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = require('./URI'); // Import db connection string
+const URI = require('../URI'); // Import db connection string
 
 mongoose.connect(URI, {
     useNewUrlParser: true, // Required option for parsing db connection string
