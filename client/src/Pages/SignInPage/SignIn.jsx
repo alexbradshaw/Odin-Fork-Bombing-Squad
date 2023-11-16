5a5a2e4b795e28a9a05cd89dce962dc6fa06d5c3
vennreddy490 <vennreddy490@gmail.com>
2023-11-15 23:45:09 -0500

    SignIn styling - batch two

--- client/src/Pages/SignInPage/SignIn.jsx
+++ client/src/Pages/SignInPage/SignIn.jsx
@@ -16,40 +16,42 @@ const Login = () => {
 
   return (
     <div id='bottomSection'> 
-        <div id='formHolder'>
-        <h2 id='test'>Welcome Back</h2>
-        <form onSubmit={submitLogin}>
-          <div>
-            <label htmlFor="userOrEmail">Username or Email:</label>
-            <input
-              class='loginInput'
-              type="text"
-              // have an expression to determine if they are using a userOrEmail
-              // or an email
-              // then, set id to the according one?
-              value={userOrEmail}
-              onChange={(e) => setuserOrEmail(e.target.value)}
-              required
-            />
+        <div className='holder'> 
+          <div id='formHolder' className='innerDivs'>
+          <h2 id='test'>Welcome Back</h2>
+          <form onSubmit={submitLogin}>
+            <div>
+              <label htmlFor="userOrEmail">Username or Email:</label>
+              <input
+                className='loginInput'
+                type="text"
+                // have an expression to determine if they are using a userOrEmail
+                // or an email
+                // then, set id to the according one?
+                value={userOrEmail}
+                onChange={(e) => setuserOrEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label  htmlFor="password">Password:</label>
+              <input
+                type="password"
+                className='loginInput'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <button id='signInButton' type="submit">Sign In</button>
+            </div>
+          </form>
+          </div> 
+          <div id='rightImage' className='innerDivs'>
+            <img src="/full.png" alt="SAVY logo" id='logo' />
           </div>
-          <div>
-            <label  htmlFor="password">Password:</label>
-            <input
-              type="password"
-              class='loginInput'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <button id='signInButton' type="submit">Sign In</button>
-          </div>
-        </form>
-      </div> 
-      <div id='rightImage'>
-        <img src="/full.png" alt="SAVY logo" id='logo' />
-      </div>
+        </div>
     </div>
 
   );
