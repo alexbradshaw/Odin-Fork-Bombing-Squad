1c6a59a5eb8d724cdeadd289903ff0ff4850a98b
vennreddy490 <vennreddy490@gmail.com>
2023-11-15 23:13:11 -0500

    Css styling for login, first batch

--- client/src/Pages/SignInPage/SignIn.jsx
+++ client/src/Pages/SignInPage/SignIn.jsx
@@ -15,36 +15,43 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={submitLogin}>
-        <div>
-          <label htmlFor="userOrEmail">Username or Email:</label>
-          <input
-            type="text"
-            // have an expression to determine if they are using a userOrEmail
-            // or an email
-            // then, set id to the according one?
-            value={userOrEmail}
-            onChange={(e) => setuserOrEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
+    <div id='bottomSection'> 
+        <div id='formHolder'>
+        <h2 id='test'>Welcome Back</h2>
+        <form onSubmit={submitLogin}>
+          <div>
+            <label htmlFor="userOrEmail">Username or Email:</label>
+            <input
+              class='loginInput'
+              type="text"
+              // have an expression to determine if they are using a userOrEmail
+              // or an email
+              // then, set id to the according one?
+              value={userOrEmail}
+              onChange={(e) => setuserOrEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label  htmlFor="password">Password:</label>
+            <input
+              type="password"
+              class='loginInput'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button id='signInButton' type="submit">Sign In</button>
+          </div>
+        </form>
+      </div> 
+      <div id='rightImage'>
+        <img src="/full.png" alt="SAVY logo" id='logo' />
+      </div>
     </div>
+
   );
 };
 
