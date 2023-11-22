8efbabe061660f6d4ff00e5ea63cb0a539702a4f
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-11-22 16:51:00 -0500

    added page to index.js for export

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -3,6 +3,6 @@ import Listings from './Listings/Listings';
 import SellItem from './SellItem/SellItem.jsx';
 import SignIn from './SignIn/SignIn.jsx';
 import SignUp from './SignUp/SignUp.jsx';
-import TestPage from './TestPage/TestPage';
+import SingleItem from './SingleItem/SingleItem';
 
-export { Landing, Listings, SellItem, SignIn, SignUp, TestPage }
+export { Landing, Listings, SellItem, SignIn, SignUp, SingleItem }
