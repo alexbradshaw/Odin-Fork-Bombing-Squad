0b3eb1922802b63416c2a789d7cf81939e351eb5
alexbradshaw <alexanderbradshaw5@gmail.com>
2023-11-28 15:33:15 -0500

    added error page

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,9 +1,10 @@
+import CheckoutPage from './CheckoutPage/CheckoutPage.jsx';
+import ErrorPage from './ErrorPage/ErrorPage';
 import Landing from './Landing/Landing';
 import Listings from './Listings/Listings';
 import SellItem from './SellItem/SellItem.jsx';
 import SignIn from './SignIn/SignIn.jsx';
 import SignUp from './SignUp/SignUp.jsx';
 import SingleItem from './SingleItem/SingleItem';
-import CheckoutPage from './CheckoutPage/CheckoutPage.jsx';
 
-export { Landing, Listings, SellItem, SignIn, SignUp, SingleItem, CheckoutPage }
+export { CheckoutPage, ErrorPage, Landing, Listings, SellItem, SignIn, SignUp, SingleItem }
