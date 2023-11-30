f64a5847e8ff8d6269d4dd0087f619fc0a613b04
vennreddy490 <vennreddy490@gmail.com>
2023-11-30 00:46:07 -0500

    fixed signUP

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -1,17 +1,18 @@
 import './SignUp.css'
 import React, { useState } from 'react';
 import { login } from '../../utils/API';
+import { signup } from '../../utils/API';
 
 const SignUp = () => {
   // State for handling user input
-  const [userOrEmail, setuserOrEmail] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [emailAddress, setEmailAddress] = useState('');
+  const [email, setEmail] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
   const submitLogin = (e) => {
     e.preventDefault(); // prevents page from refreshing
-    login({ userOrEmail, password });
+    login({ username, email, password });
   };
 
   return (
@@ -21,28 +22,28 @@ const SignUp = () => {
           <h2 id='test2'>Get Started Now</h2>
           <form onSubmit={submitLogin}>
             <div>
-              <label htmlFor="userOrEmail">Username:</label>
+              <label htmlFor="username">Username:</label>
               <input
                 className='loginInput'
                 type="text"
-                // have an expression to determine if they are using a userOrEmail
+                // have an expression to determine if they are using a username
                 // or an email
                 // then, set id to the according one?
-                value={userOrEmail}
-                onChange={(e) => setuserOrEmail(e.target.value)}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 required
               /> 
             </div>
             <div>
-              <label htmlFor="userOrEmail2">Email:</label>
+              <label htmlFor="username2">Email:</label>
               <input
                 className='loginInput'
                 type="text"
-                // have an expression to determine if they are using a userOrEmail
+                // have an expression to determine if they are using a username
                 // or an email
                 // then, set id to the according one?
-                value={emailAddress}
-                onChange={(e) => setEmailAddress(e.target.value)}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
